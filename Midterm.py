73383f1262273e25ba491f32ae1ef75dc29349cf
Souha-K <ksouhak@gmail.com>
2026-01-13 10:58:54 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -2,15 +2,43 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    return round(math.pi * ((radius) ** 2), 2)
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+        result = ''
+        
+        for i in range(n):
+
+            if (i > 0):
+                for j in range(i):
+                    result += '.'
+                
+                result += '#'
+                            
+            result += '\n'
+            
+        return result.rstrip()
+
 
 # Q3: Inverted Pyramid
-def inverted_pyramid(n):
-    return ""
+def inverted_pyramid(n):  
+    result = ''
+        
+    for i in range(n):
+
+        for j in range(i):
+            result += ' '
+
+        for k in range((n * 2) - (i * 2)):
+            result += '*'
+                    
+        result += '\n'
+    
+    result += ((n)  * ' ' + '*')
+        
+    return result.rstrip()
+
 
 # ----------------------------------------------------------------
 print(area_of_circle(5))
